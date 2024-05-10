010752bad20acf6894637113fbb9bb1f753ea791
Alban Rrahmani <alban-rr@outlook.com>
2024-05-10 10:34:25 +0200

    r

---
+++ Personal Titles/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Personal_Titles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Personal Titles");
+            int age = int.Parse(Console.ReadLine());
+            string gender = Console.ReadLine();
+            if (gender == "f")
+            {
+                if (age < 16)
+                {
+                    Console.WriteLine("Miss");
+                }
+                else
+                {
+                    Console.WriteLine("Ms");
+                }
+            }
+            else
+            {
+                if (age < 16)
+                {
+                    Console.WriteLine("Master");
+                }
+                else
+                {
+                    Console.WriteLine("Mr");
+                }
+            }
+        }
+    }
+}
