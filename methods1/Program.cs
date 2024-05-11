c3bd198fe0e0ec00bcdd4af06b53c0df19875269
Alban Rrahmani <alban-rr@outlook.com>
2024-05-11 16:40:57 +0200

    new

---
+++ methods1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace methods1
+{
+    internal class Program
+    {
+        static void PrintHeaderFooter(int n)
+        {
+            Console.WriteLine(new string('-', 2 * n));
+        }
+
+        static void PrintMiddleRow(int n)
+        {
+            Console.Write("-");
+            for (int i = 0; i < n - 1; i++)
+            {
+                Console.Write("\\/");
+            }
+            Console.Write("-");
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            PrintHeaderFooter(input);
+            for (int i = 0; i < input - 2; i++)
+            {
+                PrintMiddleRow(input);
+            }
+            PrintHeaderFooter(input);
+        }
+    }
+}
---
+++ methodsWithReturn/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace methodsWithReturn
+{
+    internal class Program
+    {
+        static string ReadFullName()
+
+        {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            return firstName + " " + lastName;
+
+        }
+        static void Main(string[] args)
+        {
+            ReadFullName();
+        }
+    }
+}
