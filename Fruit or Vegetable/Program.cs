5980eabfea36f83a2217a5f525ea3f3a1862f3bf
Alban Rrahmani <albanrr9@gmail.com>
2024-05-13 11:33:08 +0200

    Update Program.cs

--- Fruit or Vegetable/Program.cs
+++ Fruit or Vegetable/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace Fruit_or_Vegetable
+﻿using System;
+
+namespace Fruit_or_Vegetable
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var s = Console.ReadLine();
+            if (s == "banana" || s == "apple" || s == "kiwi" || s == "cherry" || s == "lemon" || s == "grapes") Console.WriteLine("Fruit");
+            else if (s == "tomato" || s == "cucmber" || s == "pepper" || s == "carrot") Console.WriteLine("Vegetable");
+            else Console.WriteLine("unknown");
         }
     }
 }
