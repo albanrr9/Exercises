ba39c041ac8f07d7fd7c248a4cc5f3305bfe618a
Alban Rrahmani <alban-rr@outlook.com>
2024-05-13 10:51:59 +0200

    new

---
+++ Fruit or Vegetable/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Fruit_or_Vegetable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Small Shop/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace Small_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine().ToLower();
+            string town = Console.ReadLine().ToLower();
+            int quantity = int.Parse(Console.ReadLine());
+            float price = 0;
+            if (town == "sofia")
+            {
+                if (product == "coffee")
+                {
+                    price = quantity * 0.5f;
+                }
+                if (product == "water")
+                {
+                    price = quantity * 0.8f;
+                }
+                if (product == "beer")
+                {
+                    price = quantity * 1.2f;
+                }
+                if (product == "sweets")
+                {
+                    price = quantity * 1.45f;
+                }
+                if (product == "peanuts")
+                {
+                    price = quantity * 1.6f;
+                }
+            }
+            if (town == "plovdiv")
+            {
+                if (product == "coffee")
+                {
+                    price = quantity * 0.4f;
+                }
+                if (product == "water")
+                {
+                    price = quantity * 0.7f;
+                }
+                if (product == "beer")
+                {
+                    price = quantity * 1.15f;
+                }
+                if (product == "sweets")
+                {
+                    price = quantity * 1.3f;
+                }
+                if (product == "peanuts")
+                {
+                    price = quantity * 1.5f;
+                }
+            }
+            if (town == "varna")
+            {
+                if (product == "coffee")
+                {
+                    price = quantity * 0.45f;
+                }
+                if (product == "water")
+                {
+                    price = quantity * 0.7f;
+                }
+                if (product == "beer")
+                {
+                    price = quantity * 1.1f;
+                }
+                if (product == "sweets")
+                {
+                    price = quantity * 1.35f;
+                }
+                if (product == "peanuts")
+                {
+                    price = quantity * 1.55f;
+                }
+            }
+            Console.WriteLine(price);
+        }
+    }
+}
