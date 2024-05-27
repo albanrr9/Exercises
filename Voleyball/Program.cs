ca2186f868ddef6f8a1491ebb484299586e8c1bf
Alban Rrahmani <albanrr9@gmail.com>
2024-05-27 14:11:25 +0200

    added new project

---
+++ Voleyball/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Read input data
+        string year = Console.ReadLine().ToLower();
+        int holidays = int.Parse(Console.ReadLine());
+        int weekendsHome = int.Parse(Console.ReadLine());
+
+        int sofiaWeekends = 48 - weekendsHome;
+        double playSofia = 3.0 * sofiaWeekends / 4 + 2.0 * holidays / 3;
+        double playTotal = playSofia + weekendsHome;
+
+        if (year.Equals("leap"))
+        {
+            playTotal = Math.Floor(playTotal * 15 / 100 + playTotal);
+        }
+        else if (year.Equals("normal"))
+        {
+            playTotal = Math.Floor(playTotal);
+        }
+        Console.WriteLine(playTotal);
+    }
+}
