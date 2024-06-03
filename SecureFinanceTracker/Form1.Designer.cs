97ab7a6b9632cea69da21c069469108f7999e7b8
Alban Rrahmani <albanrr9@gmail.com>
2024-06-03 14:22:42 +0200

    added new project

---
+++ SecureFinanceTracker/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace SecureFinanceTracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelAmount = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.labelDescription = new System.Windows.Forms.Label();
+            this.txtDate = new System.Windows.Forms.TextBox();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.btnAddIncome = new System.Windows.Forms.Button();
+            this.btnAddExpense = new System.Windows.Forms.Button();
+            this.btnViewSummary = new System.Windows.Forms.Button();
+            this.dgvTransactions = new System.Windows.Forms.DataGridView();
+            this.type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.amount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.description = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTransactions)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelAmount
+            // 
+            resources.ApplyResources(this.labelAmount, "labelAmount");
+            this.labelAmount.ForeColor = System.Drawing.Color.White;
+            this.labelAmount.Name = "labelAmount";
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(153)))), ((int)(((byte)(174)))));
+            this.txtAmount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.txtAmount, "txtAmount");
+            this.txtAmount.ForeColor = System.Drawing.Color.White;
+            this.txtAmount.Name = "txtAmount";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(153)))), ((int)(((byte)(174)))));
+            this.txtDescription.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.txtDescription, "txtDescription");
+            this.txtDescription.ForeColor = System.Drawing.Color.White;
+            this.txtDescription.Name = "txtDescription";
+            // 
+            // labelDescription
+            // 
+            resources.ApplyResources(this.labelDescription, "labelDescription");
+            this.labelDescription.ForeColor = System.Drawing.Color.White;
+            this.labelDescription.Name = "labelDescription";
+            // 
+            // txtDate
+            // 
+            this.txtDate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(153)))), ((int)(((byte)(174)))));
+            this.txtDate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.txtDate, "txtDate");
+            this.txtDate.ForeColor = System.Drawing.Color.White;
+            this.txtDate.Name = "txtDate";
+            this.txtDate.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtDate_KeyDown);
+            // 
+            // labelDate
+            // 
+            resources.ApplyResources(this.labelDate, "labelDate");
+            this.labelDate.ForeColor = System.Drawing.Color.White;
+            this.labelDate.Name = "labelDate";
+            // 
+            // btnAddIncome
+            // 
+            this.btnAddIncome.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(131)))), ((int)(((byte)(197)))), ((int)(((byte)(190)))));
+            this.btnAddIncome.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnAddIncome, "btnAddIncome");
+            this.btnAddIncome.ForeColor = System.Drawing.Color.White;
+            this.btnAddIncome.Name = "btnAddIncome";
+            this.btnAddIncome.UseVisualStyleBackColor = false;
+            this.btnAddIncome.Click += new System.EventHandler(this.btnAddIncome_Click);
+            // 
+            // btnAddExpense
+            // 
+            this.btnAddExpense.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(79)))), ((int)(((byte)(55)))));
+            this.btnAddExpense.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnAddExpense, "btnAddExpense");
+            this.btnAddExpense.ForeColor = System.Drawing.Color.White;
+            this.btnAddExpense.Name = "btnAddExpense";
+            this.btnAddExpense.UseVisualStyleBackColor = false;
+            this.btnAddExpense.Click += new System.EventHandler(this.btnAddExpense_Click);
+            // 
+            // btnViewSummary
+            // 
+            this.btnViewSummary.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(153)))), ((int)(((byte)(174)))));
+            this.btnViewSummary.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnViewSummary, "btnViewSummary");
+            this.btnViewSummary.ForeColor = System.Drawing.Color.White;
+            this.btnViewSummary.Name = "btnViewSummary";
+            this.btnViewSummary.UseVisualStyleBackColor = false;
+            this.btnViewSummary.Click += new System.EventHandler(this.btnViewSummary_Click);
+            // 
+            // dgvTransactions
+            // 
+            this.dgvTransactions.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTransactions.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.type,
+            this.amount,
+            this.description,
+            this.date});
+            resources.ApplyResources(this.dgvTransactions, "dgvTransactions");
+            this.dgvTransactions.Name = "dgvTransactions";
+            this.dgvTransactions.ReadOnly = true;
+            // 
+            // type
+            // 
+            resources.ApplyResources(this.type, "type");
+            this.type.Name = "type";
+            this.type.ReadOnly = true;
+            // 
+            // amount
+            // 
+            resources.ApplyResources(this.amount, "amount");
+            this.amount.Name = "amount";
+            this.amount.ReadOnly = true;
+            // 
+            // description
+            // 
+            resources.ApplyResources(this.description, "description");
+            this.description.Name = "description";
+            this.description.ReadOnly = true;
+            // 
+            // date
+            // 
+            resources.ApplyResources(this.date, "date");
+            this.date.Name = "date";
+            this.date.ReadOnly = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Teal;
+            this.panel1.Controls.Add(this.button3);
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.ForeColor = System.Drawing.Color.Red;
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(62)))), ((int)(((byte)(65)))));
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dgvTransactions);
+            this.Controls.Add(this.btnViewSummary);
+            this.Controls.Add(this.btnAddExpense);
+            this.Controls.Add(this.btnAddIncome);
+            this.Controls.Add(this.txtDate);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.labelDescription);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.labelAmount);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTransactions)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelAmount;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Label labelDescription;
+        private System.Windows.Forms.TextBox txtDate;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.Button btnAddIncome;
+        private System.Windows.Forms.Button btnAddExpense;
+        private System.Windows.Forms.Button btnViewSummary;
+        private System.Windows.Forms.DataGridView dgvTransactions;
+        private System.Windows.Forms.DataGridViewTextBoxColumn type;
+        private System.Windows.Forms.DataGridViewTextBoxColumn amount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn description;
+        private System.Windows.Forms.DataGridViewTextBoxColumn date;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ SecureFinanceTracker/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Security.Cryptography;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SecureFinanceTracker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadTransactions();
+        }
+
+        public string EncryptString(string plainText, string key)
+        {
+            byte[] iv = new byte[16];
+            byte[] array;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                        {
+                            streamWriter.Write(plainText);
+                        }
+                        
+                        array = memoryStream.ToArray();
+                    }
+                }
+            }
+
+            return Convert.ToBase64String(array);
+        }
+
+        public string DecryptString(string cipherText, string key)
+        {
+            byte[] iv = new byte[16];
+            byte[] buffer = Convert.FromBase64String(cipherText);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                        {
+                            return streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void AddTransaction(string type)
+        {
+            string amount = txtAmount.Text;
+            string description = txtDescription.Text;
+            string date = txtDate.Text;
+            string key = "arusha1324arusha"; // Use a secure key
+
+            string encryptedAmount = EncryptString(amount, key);
+            string encryptedDescription = EncryptString(description, key);
+            string encryptedDate = EncryptString(date, key);
+
+            string record = $"{type},{encryptedAmount},{encryptedDescription},{encryptedDate}";
+
+            File.AppendAllText("transactions.txt", record + Environment.NewLine);
+            MessageBox.Show($"{type} added successfully!");
+        }
+
+        private void LoadTransactions()
+        {
+            string[] lines = File.ReadAllLines("transactions.txt");
+            dgvTransactions.Rows.Clear();
+            string key = "arusha1324arusha"; // Use a secure key
+
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(',');
+                string type = parts[0];
+                string amount = DecryptString(parts[1], key);
+                string description = DecryptString(parts[2], key);
+                string date = DecryptString(parts[3], key);
+
+                dgvTransactions.Rows.Add(type, amount, description, date);
+            }
+        }
+
+        private void btnAddIncome_Click(object sender, EventArgs e)
+        {
+            AddTransaction("Income");
+            LoadTransactions();
+        }
+
+        private void btnAddExpense_Click(object sender, EventArgs e)
+        {
+            AddTransaction("Expense");
+            LoadTransactions();
+        }
+
+        private void btnViewSummary_Click(object sender, EventArgs e)
+        {
+            LoadTransactions();
+        }
+
+        private void txtDate_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                AddTransaction("Income");
+                LoadTransactions();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
