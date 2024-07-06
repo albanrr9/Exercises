9a3aee2970d01fb4f194cb80f02f9e5107ee9f8a
Alban Rrahmani <albanrr9@gmail.com>
2024-07-06 14:23:16 +0200

    +one project

---
+++ House/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace House
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double basse = (int)Math.Floor(n / 2f);
+            double roof = (int)Math.Ceiling(n / 2f);
+
+            var stars = 1;
+            if (n % 2 == 0)
+            {
+                stars++;
+            }
+
+            for (int i = 0; i < roof; i++)
+            {
+                var padding = (n - stars) / 2;
+                var line = new string('-', padding) + new string('*', stars) + new string('-', padding);
+                Console.WriteLine(line);
+                stars += 2;
+            }
+
+            for (int j = 0; j < basse; j++) 
+            {
+                var line = '|' + new string('*', n - 2) + '|';
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
